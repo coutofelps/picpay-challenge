42ee59dbd0fe9bfc159fcf6ee06c1b562e3fc8a2
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-05-15 18:21:58 -0300

    :white_check_mark: test: create new UserRepositoryTest and TransactionServiceTest

--- src/test/java/com/picpaychallenge/repositories/UserRepositoryTest.java
+++ src/test/java/com/picpaychallenge/repositories/UserRepositoryTest.java
@@ -1,14 +1,57 @@
 package com.picpaychallenge.repositories;
 
+import com.picpaychallenge.domain.user.User;
+import com.picpaychallenge.domain.user.UserType;
+import com.picpaychallenge.dtos.UserDTO;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
+    @Autowired
+    EntityManager entityManager;
+
+    @Autowired
+    UserRepository userRepository;
+
     @Test
-    void findByDocument() {
+    @DisplayName("Should get user from database")
+    void findByDocumentSuccessfully() {
+        String document = "valid_document";
+        UserDTO data = new UserDTO("Marcos Felipe", "Rodrigues Couto", document, new BigDecimal(10), "0709.marcoscouto@gmail.com", "123456", UserType.COMMON);
+        this.createUser(data);
+
+        Optional<User> result = this.userRepository.findByDocument(document);
+
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should not get user from database")
+    void findByDocumentUnsuccessfully() {
+        String document = "invalid_document";
+
+        Optional<User> result = this.userRepository.findByDocument(document);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    private User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.entityManager.persist(newUser);
+
+        return newUser;
     }
 }
---
+++ src/test/java/com/picpaychallenge/services/TransactionServiceTest.java
@@ -0,0 +1,19 @@
+package com.picpaychallenge.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceTest {
+
+    @Test
+    @DisplayName("Should create transaction successfully when everything is ok")
+    void createTransactionCaseOne() {
+    }
+
+    @Test
+    @DisplayName("Should throw exception when transacation authorization is not allowed")
+    void createTransactionCaseTwo() {
+    }
+}
