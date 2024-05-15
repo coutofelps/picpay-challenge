a7f94ee257cfaec93ddb461ab466002b4121e3cc
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-05-15 17:21:17 -0300

    :green_heart: ci: create test configuration folder and file

---
+++ src/test/java/com/picpaychallenge/repositories/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.picpaychallenge.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Test
+    void findByDocument() {
+    }
+}
